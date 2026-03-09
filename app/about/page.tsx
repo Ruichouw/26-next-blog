10c6c8a81113cfbef7a3721c4d2985f5e72b5f39
Ruichouw <whr888z@Gmail.com>
2026-03-09 16:46:55 +0800

    feat: 关于我页

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <main className="mx-auto max-w-4xl px-6 py-16">
-      <h1 className="text-2xl font-bold">关于我</h1>
+      <p className="text-base font-bold">这个人很神秘...</p>
     </main>
   );
 }
